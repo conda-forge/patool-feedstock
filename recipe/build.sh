231c92492e65e783ceeb7783937314e7a5252d90
Filipe <ocefpaf@gmail.com>
2018-09-24 15:21:23 -0300

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,8 @@
 
 set -e -o pipefail -x
 
-python setup.py install --single-version-externally-managed --record record.txt
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+
 mv $PREFIX/bin/patool $PREFIX/bin/patool_orig.py
 echo '#!/bin/sh' > $PREFIX/bin/patool
 echo >> $PREFIX/bin/patool
@@ -10,4 +11,3 @@ echo 'set -e -o pipefail' >> $PREFIX/bin/patool
 echo -n "python $PREFIX/bin/patool_orig.py " >> $PREFIX/bin/patool
 echo '"$@"' >> $PREFIX/bin/patool
 chmod u+x $PREFIX/bin/patool
-
